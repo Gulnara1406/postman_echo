9f470010f437c19b21ad4558a68c9e4d70bbc0b5
123 <nabgulnara@mail.ru>
2023-07-14 15:39:24 +0300

    failed test

---
+++ src/test/java/ru/netology/service/PostmanEchoTest.java
@@ -0,0 +1,24 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+class PostmanEchoTest {
+    @Test
+    void shouldReturnDate(){
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("somedata"));
+        ;
+    }
+
+}
